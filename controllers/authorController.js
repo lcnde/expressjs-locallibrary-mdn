30324e85c8f32b8883e4c2004530b4fd7c08e405
lcnde <176673026+lcnde@users.noreply.github.com>
2022-02-27 17:13:24 +0100

    feature: added delete author

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -107,12 +107,53 @@ const author_create_post = [
 
 // display Author delete form on GET
 const author_delete_get = (req, res) => {
-  res.send('NOT IMPLEMENTED: Author delete GET');
+  
+  async.parallel({
+    author: function(callback) {
+      Author.findById(req.params.id).exec(callback)
+    },
+    authors_books: function(callback) {
+      Book.find({ 'author': req.params.id }).exec(callback)
+    },
+  }, function(err, results) {
+    if (err) { return next(err); }
+    if (results.author===null) { // no results
+      res.redirect('/catalog/authors');
+    }
+    // successful so render
+    res.render('author_delete', { title: 'Delete Author', author: results.author, author_books: results.authors_books });
+  });
+
 };
 
 // display list of all authors
 const author_delete_post = (req, res) => {
-  res.send('NOT IMPLEMENTED: Author delete POST');
+  
+  async.parallel({
+    author: function(callback) {
+      Author.findById(req.body.authorid).exec(callback)
+    },
+    authors_books: function(callback) {
+      Book.find({ 'author': req.body.authorid }).exec(callback)
+    },
+  }, function(err, results) {
+    if (err) {return next(err);}
+    //success
+    if (results.authors_books.length > 0) {
+      // author has books. render in same way as for get route
+      res.render('author_delete', { title: 'Delete Author', author: results.author, author_books: results.authors_books });
+      return;
+    }
+    else {
+      // author has no books. delete object and redirect to the list of authors
+      Author.findByIdAndRemove(req.body.authorid, function deleteAuthor(err) {
+        if (err) {return next(err);}
+        // success go to author list
+        res.redirect('/catalog/authors')
+      })
+    }
+  })
+
 };
 
 // display list of all authors
